5093c13cf4e7c1704b484b13f83fe7f2a55e195c
Kauã Rodrigues <kauarodrigues.fn@gmail.com>
2026-02-09 23:06:24 -0300

    feat: criar DTOs de usuario

---
+++ src/main/java/br/edu/infnet/gestao_academica/dto/LoginRequestDTO.java
@@ -0,0 +1,4 @@
+package br.edu.infnet.gestao_academica.dto;
+
+public record LoginRequestDTO(String email, String senha) {
+}
---
+++ src/main/java/br/edu/infnet/gestao_academica/dto/UsuarioRequestDTO.java
@@ -0,0 +1,4 @@
+package br.edu.infnet.gestao_academica.dto;
+
+public record UsuarioRequestDTO(String nome, String email, String senha, String papel) {
+}
